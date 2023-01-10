3d089ce78458b23242e6e0875e7854854c188b74
Oleg_Barbashin <banksnab@gmail.com>
2023-01-10 13:41:44 +0700

    forth commit - program is correct

--- src/main/java/ru/myfarm/Main.java
+++ src/main/java/ru/myfarm/Main.java
@@ -2,18 +2,36 @@ package ru.myfarm;
 
 import ru.myfarm.details.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        AnimalFarm farmInit = new AnimalFarm(List.of(       // создали и наполнили ферму
-                "DOG Мухтар", "DOG Мухтарыч", "DOG Мухтарище",
-                "CAT Мухтар", "CAT Пушок","CAT Рыжик","CAT Чистюля","CAT Молния",
+        ArrayList<String> farm = new ArrayList<>(Arrays.asList(
+                "DOG Мухтар", "DOG Жучка",
+                "CAT Мухтар", "CAT Пушок","CAT Рыжий","CAT Чистюля","CAT Молния",
                 "HORSE Речка",
-                "COW Зорька"
-        ));
-        //farmInit.addFarmAnimal(Animal.DOG);  // -- не работает метод добавления животных в ферму
+                "COW Вечер"
+                ));
+        AnimalFarm farmInit = new AnimalFarm(farm); // создали и наполнили ферму
 
-        System.out.println("Наша ферма " + farmInit);                   // Выводим на печать нашу ферму
+        //  AnimalFarm farmInit = new AnimalFarm(List.of(
+        //          "DOG Мухтар", "DOG Мухтарыч", "DOG Мухтарище",
+        //
+        // Если создать список через List.of() или Arrays.asList(), в них уже нельзя добавить или удалить элемент.
+
+        System.out.println("Наша ферма в основании: ");
+        System.out.println(farmInit);
+
+        farmInit.addFarmAnimal(Animal.DOG);  // метод добавления животных в ферму
+        farmInit.addFarmAnimal(Animal.DOG, "Мухтарище");
+        farmInit.addFarmAnimal(Animal.CAT, "Рыжик");
+        farmInit.addFarmAnimal(Animal.HORSE, "Гнедой");
+        farmInit.addFarmAnimal(Animal.COW, "Зорька");
+        farmInit.addFarmAnimal("Чебурашка");
+        farmInit.addFarmAnimal("Гена Крокодил");
+
+        System.out.println("Наша ферма с новыми питомцами: ");                   // Выводим на печать
         System.out.println(farmInit.toString());                    // проверяем как переопределяется вывод на печать
         System.out.println("Всего на ферме живут " + farmInit.countedAnimals());  // выводим на печать количество животных каждого типа
         System.out.println("Уникальные клички " + farmInit.uniqueNames());      // выводим на печать уникальные клички
